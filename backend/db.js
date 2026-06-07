dcf9ef0745683afcece3e781acc5fcb47a21446f
efuas <efuas33@gmail.com>
2026-06-07 15:46:38 +0000

    added zod and completed basic register endpoint

---
+++ backend/db.js
@@ -0,0 +1,10 @@
+import { Pool } from "pg";
+
+export const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "devchroniclesdb",
+  password: "pa$$w0rd",
+  port: 5432,
+});
+
